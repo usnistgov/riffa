eed57f608b5e39d9390b3ae72f933c07430158bd
gabeycas <gabe.ycas@nist.gov>
2018-01-12 10:46:20 -0700

    Another reorg

---
+++ driver/windows/sys/riffa/RIFFA/x64/Windows8.1/RiffaLog.h
@@ -0,0 +1,90 @@
+
+#ifndef _RIFFALOG_
+#define _RIFFALOG_
+
+//
+//  Status values are 32 bit values layed out as follows:
+//
+//   3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+//   1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+//  +---+-+-------------------------+-------------------------------+
+//  |Sev|C|       Facility          |               Code            |
+//  +---+-+-------------------------+-------------------------------+
+//
+//  where
+//
+//      Sev - is the severity code
+//
+//          00 - Success
+//          01 - Informational
+//          10 - Warning
+//          11 - Error
+//
+//      C - is the Customer code flag
+//
+//      Facility - is the facility code
+//
+//      Code - is the facility's status code
+//
+
+//
+//  Values are 32 bit values laid out as follows:
+//
+//   3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+//   1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+//  +---+-+-+-----------------------+-------------------------------+
+//  |Sev|C|R|     Facility          |               Code            |
+//  +---+-+-+-----------------------+-------------------------------+
+//
+//  where
+//
+//      Sev - is the severity code
+//
+//          00 - Success
+//          01 - Informational
+//          10 - Warning
+//          11 - Error
+//
+//      C - is the Customer code flag
+//
+//      R - is a reserved bit
+//
+//      Facility - is the facility code
+//
+//      Code - is the facility's status code
+//
+//
+// Define the facility codes
+//
+#define FACILITY_RIFFA_ERROR_CODE        0x1
+
+
+//
+// Define the severity codes
+//
+#define STATUS_SEVERITY_SUCCESS          0x0
+#define STATUS_SEVERITY_INFORMATIONAL    0x1
+#define STATUS_SEVERITY_WARNING          0x2
+#define STATUS_SEVERITY_ERROR            0x3
+
+
+//
+// MessageId: RIFFA_INITIALIZATION_MESSAGE
+//
+// MessageText:
+//
+// RIFFA device initialized.
+//
+#define RIFFA_INITIALIZATION_MESSAGE     ((NTSTATUS)0x40010001L)
+
+//
+// MessageId: RIFFA_ACCESS_CLOSED_DMA
+//
+// MessageText:
+//
+// An attempt was made by channel %2 to write data to a closed DMA. Most likely the FPGA 
+// wrote data to a TX that has timed out.
+//
+#define RIFFA_ACCESS_CLOSED_DMA          ((NTSTATUS)0xC0010002L)
+
+#endif /* _RIFFALOG_ */
